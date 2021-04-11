ad4f074b7027f3279900795383eca271089e2fd3
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-11 21:20:47 +0530

    add logic store daily wage along with total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,14 +1,18 @@
-const Num_OF_WORKING_DAYS = 10;
+const Num_OF_WORKING_DAYS = 20;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4;
 const FUL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const MAX_HRS_IN_MONTH = 100;
+const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empHrs = 0;
+let empDailyWageArr = new Array();
 
+function calDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
 function getWorkingHours(empCheck) {
     switch (empCheck) {
 
@@ -23,9 +27,11 @@ function getWorkingHours(empCheck) {
 
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_WORKING_DAYS) {
     totalWorkingDays++;
-    let empcheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs = totalEmpHrs + getWorkingHours(empcheck);
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calDailyWage(empHrs));
 }
 
-let empwage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("total Days " + totalWorkingDays + " total hrs: " + totalEmpHrs + " Emp wage: " + empwage);
+let empWage = calDailyWage(totalEmpHrs);
+console.log("Total Days " + totalWorkingDays + " Total Hrs " + totalEmpHrs + " Emp Wage " + empWage);
