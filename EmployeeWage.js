9702bbe9832a98897d59bb1eb70f16a20c3f3601
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-11 21:18:58 +0530

    add logic calculate employee wage till condition not true

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,12 @@
-const Num_OF_WORKING_DAYS = 2;
+const Num_OF_WORKING_DAYS = 10;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2
 const PART_TIME_HOURS = 4;
 const FUL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 100;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 let empHrs = 0;
 
 function getWorkingHours(empCheck) {
@@ -17,9 +20,12 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-for (let day=0; day < Num_OF_WORKING_DAYS; day++) {
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs = empHrs + getWorkingHours(empCheck);
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < Num_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empcheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs = totalEmpHrs + getWorkingHours(empcheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs " + empHrs + " Emp Wage " + empWage)
+
+let empwage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("total Days " + totalWorkingDays + " total hrs: " + totalEmpHrs + " Emp wage: " + empwage);
