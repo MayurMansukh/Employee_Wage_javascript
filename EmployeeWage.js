a06e8179752403c06c3dab71dc47d0ab23ca5812
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-11 21:14:48 +0530

    add function calculate daily working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,18 +5,18 @@ const FUL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 let empHrs = 0;
 
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FUL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
 
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FUL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-
-let empwag = empHrs * WAGE_PER_HOUR
-console.log("Emp Wage " + empwag);
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage " + empWage);
