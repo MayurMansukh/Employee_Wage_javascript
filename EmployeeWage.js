05666f44b5526ee9b07f04a99abfed60facc49d0
manojmansukh7004 <manojmansukh7004@gmail.com>
2021-04-13 19:37:34 +0530

    add logic extend code to store gender and strt date in employeePayroll

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,23 +1,31 @@
 class EmployeePayroll{
     id;
     salary;
+    gender;
+    startdate;
 
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startdate=params[4];
     } 
 
     get name(){ return this._name;}
     set name(name){ this._name=name;}
 
     toString(){
-        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary;
+        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary+ " , gender= " +this.gender+
+        " , startdate= "+this.startdate;
     }
 }
 
-let employeePayroll = new EmployeePayroll(1,"Robin",20000);
+let employeePayroll = new EmployeePayroll(1,"rajan",80000);
 console.log(employeePayroll.toString());
 
-employeePayroll.name="Mark";
+employeePayroll.name="mahesh";
 console.log(employeePayroll.toString());
+
+let newemployeePayroll = new EmployeePayroll(2,"nilam",98000,"F",new Date("30-05-2020"));
+console.log(newemployeePayroll.toString());
